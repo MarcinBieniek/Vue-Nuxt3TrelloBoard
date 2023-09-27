b221d4e351b3239c4eaa6b863a57c44747bea690
Marcin Bieniek <marcingim@gmail.com>
2023-09-27 16:54:53 +0200

    feat: add add and delete column function

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -49,10 +49,26 @@
   ])
   const control = useKeyModifier("Control")
 
+  function createColumn() {
+    const column: Column = {
+      id: nanoid(),
+      title: "",
+      tasks: [],
+    };
+    columns.value.push(column);
+    nextTick(() => {
+      (
+        document.querySelector(
+          ".column:last-of-type .title-input"
+        ) as HTMLInputElement
+      ).focus();
+    });
+  }
+
 </script>
 
 <template>
-  <section >
+  <section class="flex items-start overflow-x-auto gap-4">
     <draggable
       v-model="columns"
       group="columns"
@@ -67,7 +83,15 @@
       >
         <header class="font-bold mb-2">
           <DragHandle />
-          {{ column.title }}
+
+          <input
+            class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+            @keyup.enter="($event.target as HTMLInputElement).blur()"
+            @keydown.backspace="column.title === '' ? columns = columns.filter( c => c.id !== column.id) : null"
+            type="text"
+            v-model="column.title"
+          />
+
         </header>
         <draggable
           v-model="column.tasks"
@@ -91,7 +115,12 @@
       </div>
     </template>
     </draggable>
-
+    <button
+      @click="createColumn"
+      class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+    >
+      + Add Another Column
+    </button>
   </section>
 </template>
 
